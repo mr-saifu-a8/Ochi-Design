90b5a2694f1c04de815773b7c47b5704d79facc8
mrsaifu08 <saifstack25@gmail.com>
2025-10-25 01:15:59 +0530

    dummy card

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -35,6 +35,195 @@ const Featured = () => {
           />
         </div>
       </div>
+      <div className="px-15 relative py-5 flex gap-3">
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          brand identity
+        </button>
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          pitch deck
+        </button>
+
+        <div className="absolute right-[31vw] flex gap-3">
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            brand identity
+          </button>
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            pitch deck
+          </button>
+        </div>
+      </div>
+      <div className="flex relative justify-center items-center gap-5 pt-25 ">
+        <div className="w-[45vw]  h-[35vw] overflow-hidden rounded-2xl bg-red-600">
+          <h1 className="absolute top-17 left-15 uppercase flex items-center gap-1  ">
+            <GoDotFill className="text-center text-2xl" />
+            salience labs
+          </h1>
+
+          <img
+            className="w-full h-full object-cover"
+            src="https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-1326x1101.png"
+            alt=""
+          />
+        </div>
+        <div className="w-[45vw] h-[35vw] overflow-hidden rounded-2xl">
+          <h1 className="absolute top-17  uppercase flex items-center gap-1  ">
+            <GoDotFill className="text-center text-2xl" />
+            salience labs
+          </h1>
+
+          <img
+            className="w-full object-cover h-full"
+            src="https://ochi.design/wp-content/uploads/2025/08/Med_Website_0.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="px-15 relative py-5 flex gap-3">
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          brand identity
+        </button>
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          pitch deck
+        </button>
+
+        <div className="absolute right-[31vw] flex gap-3">
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            brand identity
+          </button>
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            pitch deck
+          </button>
+        </div>
+      </div>
+      <div className="flex relative justify-center items-center gap-5 pt-25 ">
+        <div className="w-[45vw]  h-[35vw] overflow-hidden rounded-2xl bg-red-600">
+          <h1 className="absolute top-17 left-15 uppercase flex items-center gap-1  ">
+            <GoDotFill className="text-center text-2xl" />
+            salience labs
+          </h1>
+
+          <img
+            className="w-full h-full object-cover"
+            src="https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-1326x1101.png"
+            alt=""
+          />
+        </div>
+        <div className="w-[45vw] h-[35vw] overflow-hidden rounded-2xl">
+          <h1 className="absolute top-17  uppercase flex items-center gap-1  ">
+            <GoDotFill className="text-center text-2xl" />
+            salience labs
+          </h1>
+
+          <img
+            className="w-full object-cover h-full"
+            src="https://ochi.design/wp-content/uploads/2025/08/Med_Website_0.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="px-15 relative py-5 flex gap-3">
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          brand identity
+        </button>
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          pitch deck
+        </button>
+
+        <div className="absolute right-[31vw] flex gap-3">
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            brand identity
+          </button>
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            pitch deck
+          </button>
+        </div>
+      </div>
+      <div className="flex relative justify-center items-center gap-5 pt-25 ">
+        <div className="w-[45vw]  h-[35vw] overflow-hidden rounded-2xl bg-red-600">
+          <h1 className="absolute top-17 left-15 uppercase flex items-center gap-1  ">
+            <GoDotFill className="text-center text-2xl" />
+            salience labs
+          </h1>
+
+          <img
+            className="w-full h-full object-cover"
+            src="https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-1326x1101.png"
+            alt=""
+          />
+        </div>
+        <div className="w-[45vw] h-[35vw] overflow-hidden rounded-2xl">
+          <h1 className="absolute top-17  uppercase flex items-center gap-1  ">
+            <GoDotFill className="text-center text-2xl" />
+            salience labs
+          </h1>
+
+          <img
+            className="w-full object-cover h-full"
+            src="https://ochi.design/wp-content/uploads/2025/08/Med_Website_0.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="px-15 relative py-5 flex gap-3">
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          brand identity
+        </button>
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          pitch deck
+        </button>
+
+        <div className="absolute right-[31vw] flex gap-3">
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            brand identity
+          </button>
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            pitch deck
+          </button>
+        </div>
+      </div>
+      <div className="flex relative justify-center items-center gap-5 pt-25 ">
+        <div className="w-[45vw]  h-[35vw] overflow-hidden rounded-2xl bg-red-600">
+          <h1 className="absolute top-17 left-15 uppercase flex items-center gap-1  ">
+            <GoDotFill className="text-center text-2xl" />
+            salience labs
+          </h1>
+
+          <img
+            className="w-full h-full object-cover"
+            src="https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-1326x1101.png"
+            alt=""
+          />
+        </div>
+        <div className="w-[45vw] h-[35vw] overflow-hidden rounded-2xl">
+          <h1 className="absolute top-17  uppercase flex items-center gap-1  ">
+            <GoDotFill className="text-center text-2xl" />
+            salience labs
+          </h1>
+
+          <img
+            className="w-full object-cover h-full"
+            src="https://ochi.design/wp-content/uploads/2025/08/Med_Website_0.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="px-15 relative py-5 flex gap-3">
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          brand identity
+        </button>
+        <button className="border rounded-full py-1 px-3 uppercase text-md">
+          pitch deck
+        </button>
+
+        <div className="absolute right-[31vw] flex gap-3">
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            brand identity
+          </button>
+          <button className="border rounded-full py-1 px-3 uppercase text-md">
+            pitch deck
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
